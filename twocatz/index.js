089ad924690fe823fb60baeb7257f6d9ee07205b
Jimmy Fang <jimmyfang23@gmail.com>
2022-06-16 16:58:57 -0400

    twocatz

--- twocatz/index.js
+++ twocatz/index.js
@@ -3,12 +3,12 @@ const fetch = require('node-fetch')
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    async function GetCatPics(){
-        const resp = await fetch("https://bit-cat.azurewebsites.net/cat/says/serverless", {
+    async function GetCatPics() {
+        let resp = await fetch("https://bit-cat.azurewebsites.net/cat/says/serverless", {
             method: 'GET'
         });
         
-        const data = await resp.arrayBuffer()
+        let data = await resp.arrayBuffer()
         // we need to receive it as a buffer since this is an image we are receiving from the API
         // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
 
@@ -23,20 +23,21 @@ module.exports = async function (context, req) {
         var names = ["Shreya", "Emily", "Fifi", "Beau", "Evelyn", "Julia", "Daniel", "Fardeen"];
         var random_value = Math.floor(names.length * Math.random())
         var resultName = names[random_value]
+
         return resultName
     }
 
-    var cat1 = GetCatPics();
-    var cat1_Name = GetCatNames();
-    var cat2 = GetCatPics();
-    var cat2_Name = GetCatNames();
+    var firstCat = await GetCatPics();
+    var name1 = GetCatNames();
+    var secondCat = await GetCatPics();
+    var name2 = GetCatNames();
 
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: { 
-            cat1: cat1,
-            cat2: cat2,
-            names: [cat1_Name, cat2_Name]
+            cat1: firstCat,
+            cat2: secondCat,
+            names: [name1, name2]
          }
     };
 }
