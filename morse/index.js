57bbb9da34dfb20e949af799ea0a403faa4f3107
Jimmy Fang <jimmyfang23@gmail.com>
2022-06-16 18:58:07 -0400

    morse

--- morse/index.js
+++ morse/index.js
@@ -1,13 +1,22 @@
+const morse = require("morse-code-converter");
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const plaintext = req.query.plaintext;
+    var code = "";
+
+    if (typeof plaintext === 'undefined' || plaintext === ""){
+        code = "Please enter some text to convert"
+    }
+    else{
+        code = morse.textToMorse(plaintext);
+    }
+
+
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: code
     };
 }
